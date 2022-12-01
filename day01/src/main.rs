d3ec481f6b7fa858e17fd62be1163ae57755c212
Mathieu Poumeyrol <kali@zoy.org>
2022-12-01 08:09:39 +0100

    1

---
+++ day01/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut elves: Vec<usize> = std::fs::read_to_string("input")
+        .unwrap()
+        .split("\n\n")
+        .map(|elf| {
+            elf.trim().split("\n")
+                .map(|s| s.parse::<usize>().unwrap())
+                .sum::<usize>()
+        })
+        .collect();
+    elves.sort();
+    elves.reverse();
+    println!("p1: {:?}", elves[0]);
+    println!("p1: {:?}", elves.iter().take(3).sum::<usize>());
+}
