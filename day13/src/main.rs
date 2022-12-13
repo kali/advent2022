bed64a100564c44d1567a8be2be8c10c932db575
Mathieu Poumeyrol <kali@zoy.org>
2022-12-13 20:57:58 +0100

    bit of cleanup

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,5 +1,4 @@
 use std::cmp::Ordering;
-use std::fmt::Debug;
 
 use nom::branch::alt;
 use nom::bytes::complete::tag;
@@ -13,25 +12,6 @@ use nom::IResult;
 enum Value {
     I(i64),
     List(Vec<Value>),
-}
-
-impl Debug for Value {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        use Value::*;
-        match self {
-            I(i) => write!(f, "{}", i),
-            List(l) => {
-                write!(f, "[")?;
-                for (ix, i) in l.iter().enumerate() {
-                    if ix > 0 {
-                        write!(f, ",")?;
-                    }
-                    write!(f, "{:?}", i)?;
-                }
-                write!(f, "]")
-            }
-        }
-    }
 }
 
 fn parse(input: &str) -> IResult<&str, Value> {
@@ -49,12 +29,13 @@ impl PartialOrd for Value {
         use Value::*;
         match (self, other) {
             (I(l), I(r)) => l.partial_cmp(&r),
-            (List(l), List(r)) => Some(l
-                .iter()
-                .zip(r.iter())
-                .map(|(l, r)| l.partial_cmp(r).unwrap())
-                .find(|&it| it != Ordering::Equal)
-                .unwrap_or(l.partial_cmp(&r).unwrap())),
+            (List(l), List(r)) => Some(
+                l.iter()
+                    .zip(r.iter())
+                    .map(|(l, r)| l.partial_cmp(r).unwrap())
+                    .find(|&it| it != Ordering::Equal)
+                    .unwrap_or(l.partial_cmp(&r).unwrap()),
+            ),
             (I(_), List(_)) => List(vec![self.clone()]).partial_cmp(other),
             (List(_), I(_)) => self.partial_cmp(&List(vec![other.clone()])),
         }
@@ -78,26 +59,17 @@ fn run(input: &str) -> (usize, usize) {
     let p1: usize = pairs
         .iter()
         .enumerate()
-        .filter_map(|(ix, (l, r))| {
-            if l <= r && !(l<r)  {
-                println!("{:?}\n{:?}", l, r);
-            }
-            if l <= r {
-                Some(ix + 1)
-            } else {
-                None
-            }
-        })
+        .filter_map(|(ix, (l, r))| if l <= r { Some(ix + 1) } else { None })
         .sum();
     let a = parse("[[2]]").unwrap().1;
     let b = parse("[[6]]").unwrap().1;
-    let mut packets:Vec<Value> = pairs.into_iter().flat_map(|p| [p.0, p.1]).collect();
+    let mut packets: Vec<Value> = pairs.into_iter().flat_map(|p| [p.0, p.1]).collect();
     packets.push(a.clone());
     packets.push(b.clone());
     packets.sort();
     let a = packets.iter().position(|x| x == &a).unwrap() + 1;
     let b = packets.iter().position(|x| x == &b).unwrap() + 1;
-    (p1, a*b)
+    (p1, a * b)
 }
 
 #[test]
